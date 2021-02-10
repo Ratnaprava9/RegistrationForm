9a0d4132e01d1521beade023545e2a0f9fed0c1c
Ratnaprava9 <75732048+Ratnaprava9@users.noreply.github.com>
2021-02-10 17:52:08 +0530

    Initial commit

---
+++ RegistrationForm/Controllers/RegistrationsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationForm.Data;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        private RegistrationFormContext db = new RegistrationFormContext();
+
+        // GET: Registrations
+        public ActionResult Index()
+        {
+            return View(db.Registrations.ToList());
+        }
+
+        // GET: Registrations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,RegistrationName,Password")] Registration registration)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+
+                string message = string.Empty;
+                switch (registration.RegistrationName)
+                {
+                    case "one":
+                        message = "User name already exists";
+                        break;
+
+                    default:
+                        message="Registration successful";
+                        break;
+                }
+                ViewBag.Message = message;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registrations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,RegistrationName,Password")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registration registration = db.Registrations.Find(id);
+            db.Registrations.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RegistrationForm/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Please Enter Valid Name")]
+        public string RegistrationName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
